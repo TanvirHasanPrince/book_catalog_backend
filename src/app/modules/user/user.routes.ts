f5d71900897be401d99926ea25853f3005bbb90a
unknown <t.hasan.prince@gmail.com>
2023-09-18 15:16:32 +0600

    [Done]- Get All User-Admin

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { UserController } from './user.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
-router.get('/', UserController.getAllFromDB)
+router.get('/', auth(ENUM_USER_ROLE.ADMIN),UserController.getAllFromDB)
 
 
 export const userRoutes = router;
