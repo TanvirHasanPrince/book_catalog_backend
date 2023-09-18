6ca83088405d5bed29a96a370d39739443dac3b9
unknown <t.hasan.prince@gmail.com>
2023-09-18 14:09:23 +0600

    [Done]- Schema User Email Issue fixed

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -4,4 +4,5 @@ const router = express.Router();
 
 router.post('/signin', UserController.insertIntoDB);
 
+
 export const authRoutes = router;
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { User } from '@prisma/client';
 import prisma from '../../../shared/prisma';
 
+
 const insertIntoDB = async (data: User): Promise<User> => {
   const result = await prisma.user.create({
     data,
@@ -11,3 +12,45 @@ const insertIntoDB = async (data: User): Promise<User> => {
 export const UserService = {
   insertIntoDB,
 };
+
+// const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
+//   const { email, password } = payload;
+
+//   const isUserExist = await prisma.user.findFirst({
+//     where: {
+//       email: email,
+//     },
+//   });
+
+
+//   if (!isUserExist) {
+//     throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist');
+//   }
+
+//   if (
+//     isUserExist.password &&
+//     !(await User.isPasswordMatched(password, isUserExist.password))
+//   ) {
+//     throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
+//   }
+
+//   //create access token & refresh token
+
+//   const { id: userId, role, needsPasswordChange } = isUserExist;
+//   const accessToken = jwtHelpers.createToken(
+//     { userId, role },
+//     config.jwt.secret as Secret,
+//     config.jwt.expires_in as string
+//   );
+
+//   const refreshToken = jwtHelpers.createToken(
+//     { userId, role },
+//     config.jwt.refresh_secret as Secret,
+//     config.jwt.refresh_expires_in as string
+//   );
+
+//   return {
+//     accessToken,
+//     refreshToken,
+//   };
+// };
