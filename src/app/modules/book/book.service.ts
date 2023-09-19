b756d1aa8e6fa0829caeb290b9e7a07db1187253
unknown <t.hasan.prince@gmail.com>
2023-09-19 09:36:52 +0600

    [Done] create-book (POST) → Only Allowed For Admin

---
+++ src/app/modules/book/book.service.ts
@@ -0,0 +1,19 @@
+import { Book } from "@prisma/client";
+import prisma from "../../../shared/prisma";
+
+// Create Books
+const insertIntoDB = async (data: Book): Promise<Book> => {
+  const result = await prisma.book.create({
+    data,
+    include: {
+     category: true
+    }
+  });
+
+  return result;
+};
+
+
+export const BookService = {
+  insertIntoDB,
+};
